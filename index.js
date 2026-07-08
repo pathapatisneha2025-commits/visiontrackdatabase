ee05d7f9362de96cbe95e7f530f33f00bae98d47
Sneha-datla <datla.sneha1@gmail.com>
2026-07-08 16:24:59 +0530

    changes made

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Registration = require("./routes/storeregistration.js");
 const Patient = require("./routes/Patient.js");
+const EyeExam = require("./routes/eyeexam.js");
 
 
 
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use("/registration",Registration );
 app.use("/patient",Patient );
+app.use("/eyeexam",EyeExam );
 
 
 
