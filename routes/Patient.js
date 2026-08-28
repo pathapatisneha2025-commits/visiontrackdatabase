1228db82e193ca2b82fbbf92afc7b793b99da3c7
Sneha-datla <datla.sneha1@gmail.com>
2026-08-28 15:55:15 +0530

    changes made

--- routes/Patient.js
+++ routes/Patient.js
@@ -632,7 +632,7 @@ router.get("/superadmin", async (req, res) => {
       SELECT *
       FROM patients
       WHERE role = 'superadmin'
-      AND is_deleted = false
+      AND is_deleted = f
       ORDER BY id DESC
     `);
 
